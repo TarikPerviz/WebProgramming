2dac02df0e17885946d2112a3ad9ae7da109bd6a
Tarik.Perviz <tarik.perviz@stu.ibu.edu.ba>
2024-06-05 01:53:19 +0200

    asssss

--- backend/index.php
+++ backend/index.php
@@ -6,8 +6,6 @@ require 'rest/routes/user_routes.php';
 require 'rest/routes/contact_routes.php';
 require 'rest/routes/auth_routes.php';
 require 'rest/routes/middleware_routes.php';
-header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-    header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
 Flight::start();
 ?>
